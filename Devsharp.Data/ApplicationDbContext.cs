34bd845e37c46a93b81b1a78599a3e9fc6b12c8e
yasimortazavi <73577026+yasimortazavi@users.noreply.github.com>
2024-09-03 21:33:19 +0330

    Optimizing the process of assigning value to CreateOn

--- Devsharp.Data/ApplicationDbContext.cs
+++ Devsharp.Data/ApplicationDbContext.cs
@@ -8,27 +8,17 @@ namespace Devsharp.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        protected  override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data source=.; initial Catalog=ShopCleanArhitector;integrated security = true;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            var Entity = modelBuilder.Model.GetEntityTypes();
-            foreach (var entityType in Entity) 
-            {
-                var property = entityType.FindProperty("CreateOn");
-                if (property != null)
-                {
-                    property.ValueGenerated = Microsoft.EntityFrameworkCore.Metadata.ValueGenerated.OnAdd;
-                    property.SetDefaultValueSql("GetDate()");
-                }
-            
-            }
+            modelBuilder.SetMapDate();
             //modelBuilder.ApplyConfiguration(new CustomerMap());
             //modelBuilder.Entity<Customer>().Property(x => x.FirstName).HasMaxLength(50).IsRequired();
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         }
-     //   public DbSet<Customer>Customers { get; set; }
+        //   public DbSet<Customer>Customers { get; set; }
     }
 }
---
+++ Devsharp.Data/Extension.cs
@@ -0,0 +1,39 @@
+﻿using Devsharp.Core.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Devsharp.Data
+{
+    public static class Extension
+    {
+        public static void SetMapDate(this ModelBuilder modelBuilder)
+        {
+            var ListClasses = typeof(IDateEntity).GetAllClassNames();
+
+            var listEntities = modelBuilder.Model.GetEntityTypes()
+                .Where(p => ListClasses.Contains(p.ClrType.FullName));
+
+            foreach (var entityType in listEntities)
+            {
+                var property = entityType.FindProperty("CreateOn");
+                if (property != null)
+                {
+                    property.ValueGenerated = Microsoft.EntityFrameworkCore.Metadata.ValueGenerated.OnAdd;
+                    property.SetDefaultValueSql("GetDate()");
+                }
+
+            }
+        }
+
+        public static List<string> GetAllClassNames(this Type type)
+        {
+            return AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
+                 .Where(x => type.IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+                 .Select(x => x.FullName).ToList();
+
+        }
+    }
+}
