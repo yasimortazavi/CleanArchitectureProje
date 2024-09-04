2a5ce7f7d31feb86ed0efefef1ddac33fffa7954
yasimortazavi <73577026+yasimortazavi@users.noreply.github.com>
2024-09-04 15:48:19 +0330

    set value for EditDate

--- Devsharp.Data/ApplicationDbContext.cs
+++ Devsharp.Data/ApplicationDbContext.cs
@@ -3,6 +3,9 @@ using Devsharp.Data.Mapping;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Devsharp.Core.Interface;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq;
 
 namespace Devsharp.Data
 {
@@ -20,6 +23,17 @@ namespace Devsharp.Data
             //modelBuilder.ApplyConfiguration(new CustomerMap());
             //modelBuilder.Entity<Customer>().Property(x => x.FirstName).HasMaxLength(50).IsRequired();
         }
+        public override EntityEntry<TEntity> Update<TEntity>(TEntity entity) where TEntity : class
+        {
+            if (entity.GetType().GetInterfaces().Any(p => p.Name == "IDateEntity"))
+            {
+                Entry((entity as IDateEntity).CreateDate).State = EntityState.Unchanged;
+                (entity as IDateEntity).EditDate = DateTime.Now;
+            }
+
+            return base.Update(entity);
+        }
+
         //   public DbSet<Customer>Customers { get; set; }
     }
 }
